8a59011140967cf028f3f4fe75a71b59c93b2724
Hardeep.io <Hardeep0khalsa122@gmail.com>
2024-05-19 15:53:17 +0530

    removed build files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mergemate',
-    version='0.1.0',
+    version='0.1.1',
     author='Hardeep Singh',
     author_email='hardeep0khalsa122@gmail.com',
     packages=find_packages(),
